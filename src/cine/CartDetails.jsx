972fe4e4c551e11645fdb02f881e2f6d9d60d7b3
Mahbub Faruque Ahmed <mahbubf200@gmail.com>
2024-01-25 23:17:30 +0600

    remove cart

--- src/cine/CartDetails.jsx
+++ src/cine/CartDetails.jsx
@@ -3,8 +3,15 @@ import Delete from "../assets/delete.svg";
 import Checkout from "../assets/icons/checkout.svg";
 import { MovieContext } from "../context";
 import { getImageUrl } from "../utils/cine-utility";
+
 export default function CartDetails({ onClose }) {
   const { cartData, setCartData } = useContext(MovieContext);
+  function handleDeleteCart(e, itemId) {
+    e.preventDefault();
+    const filteredData = cartData.filter((item) => item.id !== itemId);
+    setCartData([...filteredData]);
+  }
+
   console.log(cartData);
   return (
     <div className="fixed top-0 left-0 w-screen h-screen z-50 bg-black/60 backdrop-blur-sm">
@@ -14,34 +21,41 @@ export default function CartDetails({ onClose }) {
             Your Carts
           </h2>
           <div className="space-y-8 lg:space-y-12 max-h-[450px] overflow-auto mb-10 lg:mb-14">
-            {cartData.map((item) => (
-              <div className="grid grid-cols-[1fr_auto] gap-4">
-                <div className="flex items-center gap-4">
-                  <img
-                    className="rounded overflow-hidden"
-                    src={getImageUrl(item.cover)}
-                    alt=""
-                    width={"50px"}
-                    height={"50px"}
-                  />
-                  <div>
-                    <h3 className="text-base md:text-xl font-bold">
-                      {item.title}
-                    </h3>
-                    <p className="max-md:text-xs text-[#575A6E]">
-                      {item.genre}
-                    </p>
-                    <span className="max-md:text-xs">${item.price}</span>
+            {cartData.length === 0 ? (
+              <p>cart is empty</p>
+            ) : (
+              cartData.map((item) => (
+                <div key={item.id} className="grid grid-cols-[1fr_auto] gap-4">
+                  <div className="flex items-center gap-4">
+                    <img
+                      className="rounded overflow-hidden"
+                      src={getImageUrl(item.cover)}
+                      alt=""
+                      width={"50px"}
+                      height={"50px"}
+                    />
+                    <div>
+                      <h3 className="text-base md:text-xl font-bold">
+                        {item.title}
+                      </h3>
+                      <p className="max-md:text-xs text-[#575A6E]">
+                        {item.genre}
+                      </p>
+                      <span className="max-md:text-xs">${item.price}</span>
+                    </div>
+                  </div>
+                  <div className="flex justify-between gap-4 items-center">
+                    <button
+                      className="bg-[#D42967] rounded-md p-2 md:px-4 inline-flex items-center space-x-2 text-white"
+                      onClick={(e) => handleDeleteCart(e, item.id)}
+                    >
+                      <img className="w-5 h-5" src={Delete} alt="" />
+                      <span className="max-md:hidden">Remove</span>
+                    </button>
                   </div>
                 </div>
-                <div className="flex justify-between gap-4 items-center">
-                  <button className="bg-[#D42967] rounded-md p-2 md:px-4 inline-flex items-center space-x-2 text-white">
-                    <img className="w-5 h-5" src={Delete} alt="" />
-                    <span className="max-md:hidden">Remove</span>
-                  </button>
-                </div>
-              </div>
-            ))}
+              ))
+            )}
           </div>
           <div className="flex items-center justify-end gap-2">
             <a
