fa750f362baec58e51af46c91f989cca1e088801
Mahbub Faruque Ahmed <mahbubf200@gmail.com>
2024-01-25 19:39:14 +0600

    add to cart added but when clicked on modal cart not added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
+import { useState } from "react";
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
 import SideBar from "./SideBar";
 import MovieList from "./cine/MovieList";
-
+import { MovieContext } from "./context";
 function App() {
+  const [cartData, setCartData] = useState([]);
   return (
     <>
-      <Header></Header>
-      <main>
-        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
-          <SideBar></SideBar>
-          <MovieList></MovieList>
-        </div>
-      </main>
-      <Footer></Footer>
+      <MovieContext.Provider value={{ cartData, setCartData }}>
+        <Header></Header>
+        <main>
+          <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+            <SideBar></SideBar>
+            <MovieList></MovieList>
+          </div>
+        </main>
+        <Footer></Footer>
+      </MovieContext.Provider>
     </>
   );
 }
--- src/cine/MovieCard.jsx
+++ src/cine/MovieCard.jsx
@@ -1,10 +1,12 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { MovieContext } from "../context";
 import { getImageUrl } from "../utils/cine-utility";
 import MovieDetailsModal from "./MovieDetailsModal";
 import Rating from "./Rating";
 export default function MovieCard({ movie }) {
   const [showModal, setShowModal] = useState(false);
   const [selectedMovie, setSelectedMovie] = useState(null);
+  const { cartData, setCartData } = useContext(MovieContext);
   function handleModalClose() {
     setSelectedMovie(null);
     setShowModal(false);
@@ -13,12 +15,25 @@ export default function MovieCard({ movie }) {
     setSelectedMovie(movie);
     setShowModal(true);
   }
+  function handleAddToCart(movie, event) {
+    event.stopPropagation();
+    const found = cartData.find((item) => {
+      return item.id === movie.id;
+    });
+    console.log(movie);
+    if (!found) {
+      setCartData([...cartData, movie]);
+    } else {
+      alert(`The movie ${movie.title} has been added to the cart already`);
+    }
+  }
   return (
     <>
       {showModal && (
         <MovieDetailsModal
           movie={selectedMovie}
           onClose={handleModalClose}
+          onCartAdd={handleAddToCart}
         ></MovieDetailsModal>
       )}
       <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
@@ -38,8 +53,10 @@ export default function MovieCard({ movie }) {
             <a
               className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
               href="#"
+              onClick={(e) => handleAddToCart(movie, e)}
             >
               <img src="./assets/tag.svg" alt="" />
+
               <span>${movie.price} | Add to Cart</span>
             </a>
           </figcaption>
